99e8ce11c0af79587647d696e9d6d7d88e56a587
Lena <elena.pokhvalennaya@gmail.com>
2022-02-07 22:19:49 +0200

    added task 1 in script.js file

---
+++ script.js
@@ -0,0 +1,6 @@
+// task 1
+
+const searchString = (originalStr, str) => originalStr.includes(str);
+
+console.log(searchString("Hello", "llo")); // true
+console.log(searchString("Hello", "www")); // false
