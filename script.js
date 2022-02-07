eb3fc98fdce6ded2de55887278d3e192b2cba9a4
Lena <elena.pokhvalennaya@gmail.com>
2022-02-07 22:23:03 +0200

    added task 2 in script.js file

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // task 1
 
-const searchString = (originalStr, str) => originalStr.includes(str);
+// const searchString = (originalStr, str) => originalStr.includes(str);
 
-console.log(searchString("Hello", "llo")); // true
-console.log(searchString("Hello", "www")); // false
+// console.log(searchString("Hello", "llo")); // true
+// console.log(searchString("Hello", "www")); // false
+
+// task 2
+
+const capitalizeFirstLetter = (string) =>
+  string[0].toUpperCase() + string.slice(1);
+
+console.log(capitalizeFirstLetter("hello")); // Hello
+console.log(capitalizeFirstLetter("HELLO")); // HELLO
+console.log(capitalizeFirstLetter("hElLo")); // HElLo
