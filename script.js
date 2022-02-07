d8e014ae70a4658cdf062765e75d409f2ded0ae5
Lena <elena.pokhvalennaya@gmail.com>
2022-02-07 22:35:33 +0200

    added task 3 in script.js file

--- script.js
+++ script.js
@@ -7,9 +7,21 @@
 
 // task 2
 
-const capitalizeFirstLetter = (string) =>
-  string[0].toUpperCase() + string.slice(1);
+// const capitalizeFirstLetter = (string) =>
+//   string[0].toUpperCase() + string.slice(1);
 
-console.log(capitalizeFirstLetter("hello")); // Hello
-console.log(capitalizeFirstLetter("HELLO")); // HELLO
-console.log(capitalizeFirstLetter("hElLo")); // HElLo
+// console.log(capitalizeFirstLetter("hello")); // Hello
+// console.log(capitalizeFirstLetter("HELLO")); // HELLO
+// console.log(capitalizeFirstLetter("hElLo")); // HElLo
+
+// task 3
+
+const truncate = function (str, maxlength) {
+  if (str.length > maxlength) {
+    return str.substr(0, maxlength) + "…";
+  } else {
+    return str;
+  }
+};
+console.log(truncate("Hello world", 5)); // Hello...
+console.log(truncate("Hello world", 25)); // Hello world
