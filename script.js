6b04a4089a8c5d05b1e97978410d63876b2bcc98
Lena <elena.pokhvalennaya@gmail.com>
2022-02-08 11:45:09 +0200

    added task 4 in script.js file

--- script.js
+++ script.js
@@ -16,12 +16,49 @@
 
 // task 3
 
-const truncate = function (str, maxlength) {
-  if (str.length > maxlength) {
-    return str.substr(0, maxlength) + "…";
-  } else {
-    return str;
-  }
-};
-console.log(truncate("Hello world", 5)); // Hello...
-console.log(truncate("Hello world", 25)); // Hello world
+// const truncate = function (str, maxlength) {
+//   if (str.length > maxlength) {
+//     return str.substr(0, maxlength) + "…";
+//   } else {
+//     return str;
+//   }
+// };
+// console.log(truncate("Hello world", 5)); // Hello...
+// console.log(truncate("Hello world", 25)); // Hello world
+
+// task 4
+
+const students = [
+  {
+    name: "John Smith",
+    marks: [10, 8, 6, 9, 8, 7],
+  },
+  {
+    name: "John Doe",
+    marks: [9, 8, 7, 6, 7],
+  },
+  {
+    name: "Thomas Anderson",
+    marks: [6, 7, 10, 8],
+  },
+  {
+    name: "Jean-Baptiste Emanuel Zorg",
+    marks: [10, 9, 8, 9],
+  },
+];
+
+students.forEach(function (student) {
+  const averageMark = student.marks.reduce((item, index) => {
+    return item + index / student.marks.length;
+  }, 0);
+  student.averageMark = averageMark;
+});
+console.log(students);
+const arrayOfAverage = students.map((item) => {
+  students.averageMark + " ";
+  const array = students.map(({ averageMark }) => averageMark);
+  const averageOfAllStudents = array.reduce(function (item, index) {
+    return item + index / array.length;
+  }, 0);
+  console.log(averageOfAllStudents);
+});
