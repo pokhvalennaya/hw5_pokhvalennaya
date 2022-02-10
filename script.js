210d6e09e5db2e610943d45efb7be05071ea8a8e
Lena <elena.pokhvalennaya@gmail.com>
2022-02-10 14:21:14 +0200

    updated task 4 in script.js file

--- script.js
+++ script.js
@@ -28,64 +28,73 @@
 
 // task 4
 
-// const students = [
-//   {
-//     name: "John Smith",
-//     marks: [10, 8, 6, 9, 8, 7],
-//   },
-//   {
-//     name: "John Doe",
-//     marks: [9, 8, 7, 6, 7],
-//   },
-//   {
-//     name: "Thomas Anderson",
-//     marks: [6, 7, 10, 8],
-//   },
-//   {
-//     name: "Jean-Baptiste Emanuel Zorg",
-//     marks: [10, 9, 8, 9],
-//   },
-// ];
+const students = [
+  {
+    name: "John Smith",
+    marks: [10, 8, 6, 9, 8, 7],
+  },
+  {
+    name: "John Doe",
+    marks: [9, 8, 7, 6, 7],
+  },
+  {
+    name: "Thomas Anderson",
+    marks: [6, 7, 10, 8],
+  },
+  {
+    name: "Jean-Baptiste Emanuel Zorg",
+    marks: [10, 9, 8, 9],
+  },
+];
 
-// students.forEach(function (student) {
-//   const averageMark = student.marks.reduce((item, index) => {
-//     return item + index / student.marks.length;
-//   }, 0);
-//   student.averageMark = averageMark;
-// });
-// console.log(students);
+function calculateAverageMarkOfStudent(arrayStudents) {
+  const studentsWithAverageMark = arrayStudents.slice(0);
+  studentsWithAverageMark.forEach(function (studentInformation) {
+    const sumOfMarks = studentInformation.marks.reduce(function (sum, current) {
+      return sum + current;
+    });
+    const averageMark = sumOfMarks / studentInformation.marks.length;
+    studentInformation.average = averageMark;
+  });
+  return studentsWithAverageMark;
+}
+const newStudents = calculateAverageMarkOfStudent(students);
+console.log(newStudents);
 
-// const array = students.map(({ averageMark }) => averageMark);
-// const averageOfAllStudents = array.reduce(function (item, index) {
-//   return item + index / array.length;
-// }, 0);
-// console.log(averageOfAllStudents);
+function getAverageOfAverage(arrayStudentsWithAverage) {
+  const sumOfAverage = arrayStudentsWithAverage
+    .map((informationWithAverage) => informationWithAverage.average)
+    .reduce((sum, current) => sum + current);
+  const averageOfAverage = sumOfAverage / arrayStudentsWithAverage.length;
+  return averageOfAverage;
+}
+console.log(getAverageOfAverage(newStudents));
 
 // task 5
 
-const vehicles = [
-  { make: "Honda", model: "CR-V", type: "suv", price: 24045 },
-  { make: "Honda", model: "Accord", type: "sedan", price: 22455 },
-  { make: "Mazda", model: "Mazda 6", type: "sedan", price: 24195 },
-  { make: "Mazda", model: "CX-9", type: "suv", price: 31520 },
-  { make: "Toyota", model: "4Runner", type: "suv", price: 34210 },
-  { make: "Toyota", model: "Sequoia", type: "SUV", price: 45560 },
-  { make: "Toyota", model: "Tacoma", type: "truck", price: 24320 },
-  { make: "Ford", model: "F-150", type: "truck", price: 27110 },
-  { make: "Ford", model: "Fusion", type: "sedan", price: 22120 },
-  { make: "Ford", model: "Explorer", type: "SUV", price: 31660 },
-];
+// const vehicles = [
+//   { make: "Honda", model: "CR-V", type: "suv", price: 24045 },
+//   { make: "Honda", model: "Accord", type: "sedan", price: 22455 },
+//   { make: "Mazda", model: "Mazda 6", type: "sedan", price: 24195 },
+//   { make: "Mazda", model: "CX-9", type: "suv", price: 31520 },
+//   { make: "Toyota", model: "4Runner", type: "suv", price: 34210 },
+//   { make: "Toyota", model: "Sequoia", type: "SUV", price: 45560 },
+//   { make: "Toyota", model: "Tacoma", type: "truck", price: 24320 },
+//   { make: "Ford", model: "F-150", type: "truck", price: 27110 },
+//   { make: "Ford", model: "Fusion", type: "sedan", price: 22120 },
+//   { make: "Ford", model: "Explorer", type: "SUV", price: 31660 },
+// ];
 
-const suvType = vehicles.filter(function (vehicle) {
-  return vehicle.type.toLowerCase() == "suv";
-});
-console.log(suvType);
+// const suvType = vehicles.filter(function (vehicle) {
+//   return vehicle.type.toLowerCase() == "suv";
+// });
+// console.log(suvType);
 
-const suvPriceArray = suvType.map(({ price }) => price);
-const suvPrice = suvPriceArray.reduce(function (previousValue, currentValue) {
-  return previousValue + currentValue / suvPriceArray.length;
-}, 0);
-console.log(suvPrice);
+// const suvPriceArray = suvType.map(({ price }) => price);
+// const suvPrice = suvPriceArray.reduce(function (previousValue, currentValue) {
+//   return previousValue + currentValue / suvPriceArray.length;
+// }, 0);
+// console.log(suvPrice);
 
 // task 6
 
